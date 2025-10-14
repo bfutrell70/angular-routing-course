c6d51a36d461a7f82f2dbdd22f2fd54260b44ac4
Brian Futrell <bfutrel@gmail.com>
2025-10-14 08:28:43 -0400

    module 3: Demo: hooking into Router Events

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  // occurs after ResolveEnd router event
+  constructor() {
+    console.log("In constructor");
+  }
+
+  // occurs after NavigationEnd router event, but before Scroll event
+  ngOnInit(): void {
+    console.log("On Init");
+  }
 
 }
