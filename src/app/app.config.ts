1b21c0ca0712812e71b5ce44aa09fb3ed4967234
Brian Futrell <bfutrel@gmail.com>
2025-10-16 10:17:29 -0400

    module 4: added routes to app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,14 @@
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { ApplicationConfig } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { ROUTES } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
+    // works, requires an array of routes
+    // routes are defined in app.routes.ts file
+    provideRouter(ROUTES)
   ]
 };
 
