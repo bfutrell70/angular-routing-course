9dfb3118f7bc8495fba85ab4c41e51a1ef628e5b
Brian Futrell <bfutrel@gmail.com>
2025-10-21 09:14:00 -0400

    module 10: demo - unit testing router.navigate

--- src/app/cart/cart.component.spec.ts
+++ src/app/cart/cart.component.spec.ts
@@ -11,7 +11,7 @@ import { ROUTER_TOKENS } from '../app-route.constants';
 import { signal } from '@angular/core';
 
 describe('CartComponent', () => {
-  const setup = async() => {
+  const setup = async () => {
     const mockCartService = {
       cartItemsPlusQuantity: signal([]),
       subtotal: signal(1),
@@ -20,6 +20,9 @@ describe('CartComponent', () => {
     };
 
     // mock router service and spy on navigate
+    const mockRouter = createSpyFromClass(Router, {
+      methodsToSpyOn: ['navigate']
+    });
 
     const options = {
       imports: [
@@ -36,6 +39,10 @@ describe('CartComponent', () => {
           useValue: mockCartService,
         },
         // provide mock router
+        {
+          provide: Router,
+          useValue: mockRouter
+        }
       ]
     };
 
@@ -43,14 +50,22 @@ describe('CartComponent', () => {
 
     return {
       fixture,
+      mockRouter,
     };
   }
 
-  it('calls router.navigate on close', async() => {
+  it('calls router.navigate on close', async () => {
     // setup test
+    const { mockRouter } = await setup();
 
     // find and click close button
+    const closeButton = screen.getByText('close');
+    fireEvent.click(closeButton);
 
     // expect router.navigate to have been called
+    expect(mockRouter.navigate).toHaveBeenCalledTimes(1);
+    expect(mockRouter.navigate).toHaveBeenCalledWith([{ outlets: { [ROUTER_TOKENS.CART]: null } }], {
+      queryParamsHandling: 'merge'
+    });
   });
 });
