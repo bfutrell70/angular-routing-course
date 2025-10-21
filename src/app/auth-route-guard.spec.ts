daf7a7176d09892d7f02b86a055c8bb0cb8b2681
Brian Futrell <bfutrel@gmail.com>
2025-10-21 08:50:27 -0400

    module 10: Demo - unit testing functional route guards

--- src/app/auth-route-guard.spec.ts
+++ src/app/auth-route-guard.spec.ts
@@ -4,6 +4,7 @@ import { AuthService } from './services/auth.service';
 import { Router, UrlTree } from '@angular/router';
 import { createSpyFromClass } from 'jest-auto-spies';
 import { subscribeSpyTo } from '@hirez_io/observer-spy'
+import { ROUTER_TOKENS } from './app-route.constants';
 
 describe('authRouteGuard', () => {
   const setup = (
@@ -16,17 +17,27 @@ describe('authRouteGuard', () => {
     mockAuthService.userAuth.nextWith(permissions);
 
     // mock router and spy on parseUrl to return url tree
+    const mockRouter = createSpyFromClass(Router, {
+      methodsToSpyOn: ['parseUrl']
+    });
+    const urlTree = new UrlTree();
+    mockRouter.parseUrl.mockReturnValue(urlTree);
+    // END mock router and spy on parseUrl to return url tree
 
     TestBed.configureTestingModule({
       providers: [
-        { provide: AuthService, useValue: mockAuthService},
-        // provide mock router
+        { provide: AuthService, useValue: mockAuthService },
+        {provide: Router, useValue: mockRouter}
       ]
     });
 
     // instantiate route guard in test bed injection context
+    const guard = TestBed.runInInjectionContext(authRouteGuard(path));
 
     return {
+      guard,
+      urlTree,
+      mockRouter
     };
   }
   it('returns true when permission exists', () => {
@@ -34,10 +45,13 @@ describe('authRouteGuard', () => {
     const permissions = [path, 'other permission'];
 
     // setup
+    const { guard } = setup(path, permissions);
 
     // subscribe to guard
+    const guardSpy = subscribeSpyTo(guard);
 
     // expect guard to return true
+    expect(guardSpy.getLastValue()).toBe(true);
   });
 
   it('returns UrlTree when permission does not exist', () => {
@@ -45,9 +59,13 @@ describe('authRouteGuard', () => {
     const permissions = ['other permission'];
 
     // setup
+    const { guard, urlTree, mockRouter } = setup(path, permissions);
 
     // subscribe to guard
+    const guardSpy = subscribeSpyTo(guard);
 
     // expect guard to return a url tree
+    expect(guardSpy.getLastValue()).toEqual(urlTree);
+    expect(mockRouter.parseUrl).toHaveBeenCalledWith(`/${ROUTER_TOKENS.NOT_AUTH}`);
   });
 })
