4e307ed74282d262881157235dcd0a44581724e5
Brian Futrell <bfutrel@gmail.com>
2025-10-21 09:38:09 -0400

    module 10: demo - unit testing RouterLinks with RouterTestingHarness

--- src/app/products-view/side-menu/side-menu.component.spec.ts
+++ src/app/products-view/side-menu/side-menu.component.spec.ts
@@ -31,13 +31,37 @@ describe('SideMenuComponent', () => {
           useValue: mockPieService
         },
         // provide location mocks
+        provideLocationMocks(),
         // provide router with mock routes
+        provideRouter([
+          {
+            path: PRODUCT_ROUTER_TOKENS.CUSTOMIZE,
+            component: DummyComponent
+          },
+          {
+            path: PRODUCT_ROUTER_TOKENS.DETAIL,
+            component: DummyComponent
+          },
+          // must provide a route that returns the component under test
+          // must be last or it will match all of the other routes
+          {
+            path: '**',
+            component: SideMenuComponent
+          },
+        ])
       ]
     });
     // implement RouterTestingHardness
+    const harness = await RouterTestingHarness.create();
+    const activatedComponent = await harness.navigateByUrl('/', SideMenuComponent);
+
     // get location reference
+    const location = TestBed.inject(Location);
 
     return {
+      harness,
+      activatedComponent,
+      location
     };
   }
 
@@ -46,10 +70,14 @@ describe('SideMenuComponent', () => {
     const pie1 = createPie({id: pieId1});
 
     // setup
+    const { location } = await setup([pie1]);
 
     // get button and click it
+    const pieButton = screen.getByTestId(`detail-view-${pieId1}`);
+    fireEvent.click(pieButton);
 
     // expect location to change
+    expect(location.path()).toEqual(`/detail?productId=${pieId1}`);
   });
 
   it('navigates to the detail view when customize is true', async() => {
@@ -57,10 +85,16 @@ describe('SideMenuComponent', () => {
     const pie1 = createPie({id: pieId1});
 
     // setup
+    const { location, activatedComponent, harness } = await setup([pie1]);
+    activatedComponent.customize = true;
+    harness.detectChanges();
 
     // get button and click it
+    const pieButton = screen.getByTestId(`customize-view-${pieId1}`);
+    fireEvent.click(pieButton);
 
     // expect location to change
+    expect(location.path()).toEqual(`/customize?productId=${pieId1}`);
   });
 
 });
